529a46000534e686405226bc0a175560679b2561
Aaquib kuwari <aaquib.kuwari@gmail.com>
2017-08-22 00:06:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/Aaquib_kuwari_image.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Aaquib_kuwari_image.jpg'));
 });
 
 
