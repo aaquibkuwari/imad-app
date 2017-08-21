2ad950b07cffd16f813d39dbc099d180e1962f62
Aaquib kuwari <aaquib.kuwari@gmail.com>
2017-08-22 01:28:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,9 +71,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
 app.get('/counter', function (req, res) {
-    
-    res.send("counter.toString()");
+    counter = counter + 1;
+    res.send(counter.toString());
 });  
 
 
