bc15054db0487c5266449cbceac90dc0e6108737
Aaquib kuwari <aaquib.kuwari@gmail.com>
2017-08-24 14:19:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,25 @@
 console.log('Loaded!');
 // counter code
-var counter = 0;
+
 var button = document.getElementById("counter");
 button.onclick = function (){
-    //make a request to counter end point
+    //create a request
+    var request = new XMLHttpRequest();
     
-    //capture the response ans save it in a variable
+    request.onreadystatechange =  function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
     
-    //render the variable to correct span
-    counter= counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //make the request
+    request.open('GET','http://aaquibkuwari.imad.hasura-app.io/counter', true);
+    request.send(null);
+   
+    
 };
