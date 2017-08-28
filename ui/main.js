8414b1cc933c298a7244944059806f3e46d51373
Aaquib kuwari <aaquib.kuwari@gmail.com>
2017-08-29 00:48:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,5 +25,15 @@ button.onclick = function (){
 };
 
 var nameinput=document.getElementById("name");
-var name=nameinput.value;
+var name = nameinput.value;
 var submit = document.getElementById("submit_btn");
+submit.onclick = function() {
+    
+    var name=["name1","name2","name3"];
+    var list="";
+    for(var i=0; i<name.length;i++){
+        list += '<li>'+name[i]+'</li>';
+    }
+    var ul = document.getElementById("namelist");
+    ul.innerHTML=list;
+}
