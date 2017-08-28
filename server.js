cf87bcab8923a1ca4cf1413dd3db902bd3ef8849
Aaquib kuwari <aaquib.kuwari@gmail.com>
2017-08-29 00:59:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,15 @@ app.get('/ui/Aaquib_kuwari_image.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Aaquib_kuwari_image.jpg'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+  var name = req.params.name;
+  
+  names.push(name);
+  
+  res.send(JSON.stringify(names));
+});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
